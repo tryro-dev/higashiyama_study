bce87d25554863407da764cd256a2d68ffaf8ca8
Higashiyama Harutomo <h.higashiyama@tryro.co.jp>
2026-08-19 17:50:59 +0900

    初期ファイル追加

---
+++ src/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>おみくじアプリ</title>
+    </head>
+
+    <body>
+        <div class="main">
+            <h1>おみくじアプリ</h1>
+            <div class="form">
+                <label>
+                    名前を入力してください。
+                    <input type="text" placeholder="太郎">
+                </label>
+                <button type="submit">占う</button>
+            </div>
+        </div>
+        
+    </body>
+
+</html>
---
+++ src/result.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>おみくじ結果</title>
+    </head>
+
+    <body>
+        <div class="main">
+            <h1>おみくじ結果</h1>
+            <div class="result">
+                
+
+            </div>
+
+
+        </div>
+        
+    </body>
+
+</html>
