3f24d4d4f612536ba8ae915dd4af53716e1365d0
Higashiyama Harutomo <h.higashiyama@tryro.co.jp>
2026-08-20 17:31:54 +0900

    8/20更新分１

--- src/index.php
+++ src/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="ja">
+
     <head>
         <meta charset="UTF-8">
         <title>おみくじアプリ</title>
@@ -7,18 +8,18 @@
     </head>
 
     <body>
-        <div class="main">
+        <div class="main1">
             <h1>おみくじアプリ</h1>
             <div class="form">
-                <label>
+                <form action="result.php" method="post">
+                  <label>
                     名前を入力してください。
-                    <input type="text" placeholder="太郎">
-                </label>
-                <button type="submit">占う</button>
-                
+                    <input type="text" name="username" placeholder="太郎" maxlength="20" minlength="1" required>
+                  </label>
+                  <button type="submit" >占う</button>
+                </form>
             </div>
         </div>
-        
     </body>
 
 </html>
--- src/result.php
+++ src/result.php
@@ -1,31 +1,54 @@
+<?php
+ //　名前の受け取り
+ $name = $_POST['username'];
+
+ // おみくじの結果をランダムに1つ抽出
+ function omikuji() {
+    $results = ["大吉", "中吉", "小吉", "凶"];
+    return $results[array_rand($results)];
+ }
+ $fortune = omikuji();
+
+ // 結果に応じたコメント
+ function getComment($fortune) {
+    if ($fortune === "大吉") return "おめでとう!";
+    if ($fortune === "凶") return "残念!";
+    return "";
+ }
+ $comment = getComment($fortune);
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="ja">
     <head>
         <meta charset="UTF-8">
         <title>おみくじ結果</title>
+        <link rel="stylesheet" href="stylesheet.css">
     </head>
 
     <body>
-        <div class="main">
+        <div class="main2">
             <h1>おみくじ結果</h1>
             <div class="result">
-                <p>こんにちは、<?php ?>さん</p>
-                <div  class="result text">
-                    <p>あなたの運勢は「<?php ?>」です</p>
-                    <p><?php ?></p><!--大吉or凶の時のみ-->
+              <?php if(isset($_POST['username'])&& $_POST['username']!==""):?><!--名前入力時-->
+                <div class="success">
+                  <p>こんにちは、<?php echo $name ?>さん</p>
+                    <div  class="fortunetxt">
+                      <p>あなたの運勢は「<?php echo $fortune ?>」です</p>
+                      <p><?php echo $comment ?></p><!--大吉or凶の時のみ-->
+                    </div>
+                  <a href="index.php">もう一度占う</a>
                 </div>
-                <a href="">もう一度占う</a>
+              <?php else:?><!--未入力時-->
+                <div class="error">
+                  <p>名前を入力してください。</p>
+                  <a href="index.php">戻る</a>
+                </div>
+              <?php endif;?>
             </div>
-
-
-            <div class="error"><!--名前未入力時-->
-                <p>名前を入力してください。</p>
-                <a href="">戻る</a>
-            </div>
-
-
         </div>
-        
     </body>
 
 </html>
